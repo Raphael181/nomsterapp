3ea7e8f3f034f209d949c6bc54b8743d9409b96e
Raphael Madu <madur0392@gmail.com>
2019-11-28 03:01:10 +0000

    making the signing in and out more efficient

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-    before_action :authenticate_user!, only: [:new, :create]
+    before_action :authenticate_user!, only: [:new, :create, :edit, :destroy] #this line is what allows our logged in user to create, delete and update the places the user created
 
     def index
         @places = Place.all
@@ -11,7 +11,7 @@ class PlacesController < ApplicationController
     end
     
     def create
-        current_user.plaaces.create(place_params)
+        current_user.places.create(place_params)
         redirect_to root_path
     end
     
@@ -20,6 +20,28 @@ class PlacesController < ApplicationController
     end
     
     def edit
+        @place = Place.find(params[:id])
+        
+        if @place.user != current_user
+            return render plain: 'You are not authorized to do such', status: :forbidden    #this code tells us that no other user except the user who created it can either edit or delete the entered place
+        end
+    end
+    
+    def update
+     @place = Place.find(params[:id])
+     @place.update_attributes(place_params)
+     redirect_to root_path
+    end
+    
+    def destroy
+        @place = Place.find(params[:id])
+        
+        if @place.user != current_user
+            return render plain: 'You are not authorized to do such', status: :forbidden    #this code tells us that no other user except the user who created it can either edit or delete the entered place
+        end
+        
+        @place.destroy
+        redirect_to root_path
     end
     
     private
