d174f835625296bb5ff1138f796cf71ceacd9da6
Raphael Madu <madur0392@gmail.com>
2019-11-28 05:05:27 +0000

    Addinf user functionality to this

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -11,8 +11,12 @@ class PlacesController < ApplicationController
     end
     
     def create
-        current_user.places.create(place_params)
-        redirect_to root_path
+        @plzce = current_user.places.create(place_params)
+        if @place.valid?
+            redirect_to root_path
+        else
+            render :new, status: :unprocessable_entity
+        end
     end
     
     def show
@@ -27,13 +31,27 @@ class PlacesController < ApplicationController
         end
     end
     
+ 
+ 
     def update
      @place = Place.find(params[:id])
-     @place.update_attributes(place_params)
-     redirect_to root_path
+     
+     if @place.user != current_user
+        return render plain: 'You are not authorized to do such', status: :forbidden
     end
     
-    def destroy
+    @place.update_attributes(place_params)
+     
+     if @place.valid?
+        redirect_to root_path
+    else
+        render :edit, status: :unprocessable_entity
+    end 
+    end
+    
+  
+  
+  def destroy
         @place = Place.find(params[:id])
         
         if @place.user != current_user
